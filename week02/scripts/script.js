05f05aa2c63ec96c530be27b95d0e29c1d1644b9
Krista Berry <kilroy6677@gmail.com>
2025-07-09 22:25:44 -0500

    Updating With Week02 Learning Activities DOM

---
+++ week02/scripts/script.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.getElementById('list');
+
+
+
+
+button.addEventListener('click', function () {
+    //Code to execute when button is clicked
+    if (input.value.trim() == '') {
+        input.focus();
+    }
+    else {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li); 
+            input.focus();
+        });
+    }
+    input.value = ''
+    input.focus();
+});
+
+
+
